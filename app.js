1c75dc806c2aa262633c802a1f16ac0938e05a68
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-08-01 15:24:51 +0900

    js + conditionals

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
 const hello = document.querySelector(".hello:first-child h1")
 
 
-function handleBackgroundSize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
 function handleClick () {
-    hello.style.color = "blue";
+    const CurrentColor = hello.style.color;
+    let Newcolor;
+    if(CurrentColor === "blue") {
+        Newcolor = "tomato";
+    } else {
+        Newcolor = "blue";
+    }
+     hello.style.color = Newcolor ;
 }
 
-function handleMouseOn () {
-    hello.innerText = "mouse is on!"
-}
-function handleMouseDown () {
-    hello.innerText = "mouse is gone!"
-}
-
-function handleCopy () {
-    alert("copier!!");
-}
-function handleoffline(){
-    alert("SOS No wifi!!");
-}
-
-function handleonline () {
-    alert("all gooood*^^*!");
-}
-
-hello.onclick = handleClick;
-hello.addEventListener("mouseenter",handleMouseOn);
-hello.addEventListener("mouseleave",handleMouseDown);
-hello.addEventListener("copy",handleCopy);
-
-
-window.addEventListener("offline",handleoffline);
-window.addEventListener("online",handleonline);
-window.addEventListener("resize",handleBackgroundSize);
+    hello.onclick = handleClick;
