36c99b1a7117223c841739668e56c40185d0a09e
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-08-01 15:46:32 +0900

    update js
    같은 코드를 js만의 기능으로 좀 더 간결하게 적음

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleClick() {
-    h1.className = "active";
+    const current = h1.className;
+    let newcolor; 
+    if(current === "active") {
+        newcolor = "";
+    } else {
+        newcolor="active";
+    }
+    h1.className = newcolor;
 }
 
 h1.addEventListener("click",handleClick);
