951e7a3778cd343edeb6fb61d8567215c93daa37
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-08-03 01:23:17 +0900

    toggle function

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const h1 = document.querySelector(".hello h1");
 
 function handleClick() {
    const clickedClass = "clicked" // 이렇게 변수에다가 저장해두고 쓰는 게 아주 유용함!!
-
-    if(h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click",handleClick);
