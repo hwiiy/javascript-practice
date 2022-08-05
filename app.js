08e7ca22da74eddd793145cab1b4208c7a5e5070
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-08-05 17:19:37 +0900

    js challenge Assignment 5
    Completed!!

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ function handle() {
     var wframe = window.innerWidth;
 
     console.log(wframe);
-    h1.classList.add("red","blue")
+    h1.classList.add("red","blue","gold")
 
     if (wframe <= 1000){
-        h1.classList.remove("blue") //red 가 적용
+        h1.classList.remove("blue","gold") //red 가 적용
     } else if(wframe >1000 && wframe <=1500) {
-        h1.classList.remove("red");
-        h1.classList.add("blue")
+        h1.classList.add("blue")  // blue 적용
+        h1.classList.remove("red","gold"); //red 지움
     } else {
-        h1.classList.remove("red","blue")
+        h1.classList.remove("blue")
+        h1.classList.add("gold")
     }
 }
 
