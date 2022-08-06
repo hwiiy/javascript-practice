293e5510966e2ba81428a79485a629a85df81173
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-08-07 00:22:45 +0900

    시계 만들기 2
    00:00:00 으로 되게 설정 했음
    padStart() 사용!

--- clock/js/clock.js
+++ clock/js/clock.js
@@ -2,7 +2,10 @@ const clock = document.getElementById("clock");
 
 function showingClock(){
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    const hours = String(date.getHours()).padStart(2,"0")
+    const minutes = String(date.getMinutes()).padStart(2,"0")
+    const seconds = String(date.getSeconds()).padStart(2,"0")
+    clock.innerText = `${hours}:${minutes}:${seconds}`
 }
-showingClock();
+showingClock(); //브라우저 창 열리자마자 바로 시계 띄우기 위해서
 setInterval(showingClock, 1000) //1초 마다 showingClock 함수 실행시킨다!
