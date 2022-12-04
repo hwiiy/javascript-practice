5106569818c545c1a9899e679b9a4df31c860b21
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-12-04 23:13:03 +0900

    code Review : 다 지우고 복습

--- CoAlNu/second-project/main.js
+++ CoAlNu/second-project/main.js
@@ -1,79 +1,73 @@
 const taskInput = document.querySelector(".task-input");
 const addBtn = document.querySelector(".button-add");
 
+
+addBtn.addEventListener("click",addTask)
 let taskList = [];
 
-addBtn.addEventListener("click", addTask);
-
-
-
-function addTask() {
- 
+function addTask(){
     let task = {
-        id : randomIDgenerate(),
-        taskContent : taskInput.value,
-        isComplete : false
+        id:randomIDgenerate(),
+        content : taskInput.value,
+        isComplete:false,
     }
-    taskList.push(task);
-    console.log(taskList)
+
+    taskList.push(task)
+    taskInput.value = ""
     render();
-    taskInput.value="";
-}
-
-function render() {
-    let resultHTML = '';
-  
-    for (let i = 0; i < taskList.length; i++) {
-       
-
-        if(taskList[i].isComplete == true){
-            resultHTML += `
-            <div class="task">
-                        <div class="task-done">${taskList[i].taskContent}</div>
-                        <div>
-                            <button onClick="toggleComplete('${taskList[i].id}')">Check</button>
-                            <button onClick="deleteTask('${taskList[i].id}')">Delete</button>
-                        </div>
-                    </div>
-            `
-        }else {
-            resultHTML += `
-            <div class="task">
-                        <div>${taskList[i].taskContent}</div>
-                        <div>
-                            <button onClick="toggleComplete('${taskList[i].id}')">Check</button>
-                            <button onClick="deleteTask('${taskList[i].id}')">Delete</button>
-                        </div>
-                    </div>
-            `
-        }
-    }
-    document.getElementById("task-board").innerHTML = resultHTML;
 }
 
 function randomIDgenerate(){
     return Math.random().toString(36).substr(2, 16);
 }
-function toggleComplete(id){
-  
+
+
+function render(){
+    let resultHTML = "";
     for(let i=0;i<taskList.length;i++){
-        if(taskList[i].id === id){
-            // taskList[i].isComplete = true; 만약 이렇게 써버리면 평생 true인 상태로 굳음
-            taskList[i].isComplete = !taskList[i].isComplete;
-            break;
+        if(taskList[i].isComplete == true){
+            resultHTML += `
+        <div class="task">
+        <!-- task content -->
+        <div class="task-done">${taskList[i].content}</div>
+        <!-- task button -->
+        <div>
+            <button onClick="checkTask('${taskList[i].id}')">check</button>
+            <button onClick="deleteTask('${taskList[i].id}')">delete</button>
+        </div>
+        </div>`
         }
+    else {
+        resultHTML += `
+        <div class="task">
+        <!-- task content -->
+        <div>${taskList[i].content}</div>
+        <!-- task button -->
+        <div>
+            <button onClick="checkTask('${taskList[i].id}')">check</button>
+            <button onClick="deleteTask('${taskList[i].id}')">delete</button>
+        </div>
+        </div>`
     }
-    //여기서 render 다시 안 불러주면 ui 수정 되지 않음! ui그려주는 건 render 관할임.
-   render();
+        document.getElementById("task-board").innerHTML = resultHTML;
+    }
 }
 
 function deleteTask(id){
+for(let i=0;i<taskList.length;i++){
+    if(taskList[i].id ===id){
+        taskList.splice(i,1);
+    }
+}
+render();
+}
+
+function checkTask(id){
     for(let i=0;i<taskList.length;i++){
         if(taskList[i].id === id){
-            taskList.splice(i,1)
+            taskList[i].isComplete = !taskList[i].isComplete;
         }
     }
-    //여기서 render 다시 안 불러주면 ui 수정 되지 않음! ui그려주는 건 render 관할임.
-   render();
-   console.log("delete",id)
+    render();
 }
+
